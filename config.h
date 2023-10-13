93d215f8265c660365acfc490f2c2fc1920b8c26
Bakar Chargeishvili <bakar@bcharge.de>
2023-10-13 16:14:31 +0200

    boxdraw and editscreen

--- config.h
+++ config.h
@@ -57,6 +57,18 @@ static unsigned int blinktimeout = 800;
 static unsigned int cursorthickness = 2;
 
 /*
+ * 1: render most of the lines/blocks characters without using the font for
+ *    perfect alignment between cells (U2500 - U259F except dashes/diagonals).
+ *    Bold affects lines thickness if boxdraw_bold is not 0. Italic is ignored.
+ * 0: disable (render all U25XX glyphs normally from the font).
+ */
+const int boxdraw = 0;
+const int boxdraw_bold = 0;
+
+/* braille (U28XX):  1: render as adjacent "pixels",  0: use font */
+const int boxdraw_braille = 0;
+
+/*
  * bell volume. It must be a value between -100 and 100. Use 0 for disabling
  * it
  */
@@ -227,7 +239,8 @@ static Shortcut shortcuts[] = {
 	{ MODKEY,               XK_x,           zoom,           {.f = -1} },
 	{ MODKEY,               XK_z,           zoom,           {.f = +1} },
 	{ MODKEY,               XK_p,           selpaste,       {.i =  0} },
-	{ MODKEY,               XK_o,           clippaste,      {.i =  0} },
+	{ MODKEY,               XK_P,           clippaste,      {.i =  0} },
+	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_A,           chalpha,        {.f = +0.1} },
 	{ TERMMOD,              XK_D,           chalpha,        {.f = -0.1} },
 	{ MODKEY,               XK_v,           externalpipe,   {.v = copymode } }
--- editscreen.sh
+++ editscreen.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
-tmpfile=$(mktemp /tmp/st-edit.XXXXXX)
-trap  'rm "$tmpfile"' 0 1 15
-cat > "$tmpfile"
-st -e "$EDITOR" "$tmpfile"
+# Using external pipe with st, give a dmenu prompt of recent commands,
+# allowing the user to copy the output of one.
+# xclip required for this script.
+# By Jaywalker and Luke
+tmpfile=$(mktemp /tmp/st-cmd-output.XXXXXX)
+trap 'rm "$tmpfile"' 0 1 15
+gsed -n "w $tmpfile"
+gsed -i 's/\x0//g' "$tmpfile"
+#ps1="$(grep "\S" "$tmpfile" | tail -n 1 | gsed 's/^\s*//' | cut -d' ' -f1)"
+ps1=">>"
+chosen="$(grep -F "$ps1" "$tmpfile" | gsed '$ d' | tail -r | dmenu -p "Copy which command's output?" -i -l 10 | gsed 's/[^^]/[&]/g; s/\^/\\^/g')"
+eps1="$(echo "$ps1" | gsed 's/[^^]/[&]/g; s/\^/\\^/g')"
+gawk "/^$chosen$/{p=1;print;next} p&&/$eps1/{p=0};p" "$tmpfile" | tail -n +2 | xclip -r -selection clipboard
+
+#tmpfile=$(mktemp /tmp/st-edit.XXXXXX)
+#trap  'rm "$tmpfile"' 0 1 15
+#cat > "$tmpfile"
+#st -e "$EDITOR" "$tmpfile"
